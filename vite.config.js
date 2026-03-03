bed12d80bd23903dd5eef5094257d48255c72eb4
Ashly <thegirlnextfloor@gmail.com>
2026-03-02 21:23:05 -0800

    N/A

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,14 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
 export default defineConfig({
   plugins: [react()],
-});
+
+  // 👇 This is the critical line for GitHub Pages
+  base: "/presentation-slide-editor/",
+
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+  }
+})
